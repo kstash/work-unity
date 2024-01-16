fe383ba92c51864bf43a2fba1b0264e7e4671441
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-01-16 18:52:27 +0900

    [feat] add swagger module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { StorageDriver, initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
 
+  const app = await NestFactory.create(AppModule);
   const serverConfig = config.get('server');
   const port = serverConfig.port;
+
+  const document = SwaggerModule.createDocument(app, serverConfig);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(port);
   
