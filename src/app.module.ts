41f38286f48fa7137dd02e6a730c88023f8adc9f
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-01-16 18:52:05 +0900

    [fix] app module (import resource module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
+import { typeORMConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { GroupModule } from './group/group.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { UserModule } from './user/user.module';
+import { ResourceModule } from './resource/resource.module';
+import { addTransactionalDataSource } from 'typeorm-transactional';
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { UserModule } from './user/user.module';
     GroupModule,
     ScheduleModule,
     UserModule,
+    ResourceModule,
   ],
 })
 export class AppModule {}
