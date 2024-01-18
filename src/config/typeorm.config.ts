37c5cf7890baa6205659371b376b50f76c2ebee7
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-01-18 17:14:47 +0900

    [fix] connect with RDS by ssl with bundle pem key(on ec2)

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import * as config from "config";
+import * as fs from "fs";
 
 const dbConfig = config.get('db');
 
@@ -12,4 +13,14 @@ export const typeORMConfig: TypeOrmModuleOptions = {
     database: process.env.DB_NAME || dbConfig.database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: Boolean(process.env.DB_SYNC) || dbConfig.synchronize,
+    // logging: true,
+    ssl: {
+        // https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.SSL.html#UsingWithRDS.SSL.CertificatesAllRegions
+        ca: fs.readFileSync(process.env.DB_CA || dbConfig.ca)
+    },
+    extra: {
+        // SSL 연결 강제 설정
+        ssl: { rejectUnauthorized: false }
+    },
+    autoLoadEntities: true,
 }
