eaa67faa4fb9b0d8d2adc75287d5d5ed2d172d40
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-02-04 22:32:06 +0900

    [fix] apply ApiProperty decorators for Team entity

--- src/group/entity/team.entity.ts
+++ src/group/entity/team.entity.ts
@@ -11,21 +11,30 @@ import {
 } from 'typeorm';
 import { Company } from './company.entity';
 import { Profile } from 'src/user/enitity/profile.entity';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { faker } from '@faker-js/faker';
 
 @Entity('team')
-@Unique('unique_company_team_name_restraint', ['name', 'company'])
+@Unique('create_team_restraint', ['company', 'name'])
 export class Team extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
   // -------------------------------------------------------------------------
+  @ApiProperty({ description: '소속 회사', type: () => Company })
   @ManyToOne(() => Company, (company) => company.teams)
   company: Company;
   // -------------------------------------------------------------------------
+  @ApiProperty({ description: '팀 이름', example: faker.commerce.department() })
   @Column()
   name!: string;
   // -------------------------------------------------------------------------
+  @ApiProperty({ description: '생성일자', example: faker.date.past() })
   @CreateDateColumn()
   createdAt: Date;
+  @ApiPropertyOptional({
+    description: '수정일자',
+    example: faker.date.recent(),
+  })
   @UpdateDateColumn()
   updatedAt: Date;
   // -------------------------------------------------------------------------
