224f372349224cb0abccccd651c9ee7b882165c1
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-02-10 17:20:46 +0900

    [fix] validation arguments aren't used

--- src/group/validator/company-id.validator.ts
+++ src/group/validator/company-id.validator.ts
@@ -9,7 +9,7 @@ import { GroupService } from '../group.service';
 export class IsValidCompanyId implements ValidatorConstraintInterface {
   constructor(private readonly groupService: GroupService) {}
 
-  async validate(companyId: number, args: ValidationArguments) {
+  async validate(companyId: number) {
     // groupService를 사용하여 companyId가 존재하는지 확인
     const company = await this.groupService.getCompany(companyId);
     return !!company;
