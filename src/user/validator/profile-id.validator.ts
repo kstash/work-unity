86109caa6ddb15a18f12121038e2adfef8671914
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-06-21 17:37:03 +0900

    [fix] use unkown rather Object

--- src/user/validator/profile-id.validator.ts
+++ src/user/validator/profile-id.validator.ts
@@ -33,7 +33,7 @@ export class IsValidProfileId implements ValidatorConstraintInterface {
 }
 
 export const IsValidProfile = (validationOptions?: ValidationOptions) => {
-  return (object: Object, propertyName: string) => {
+  return (object: unknown, propertyName: string) => {
     registerDecorator({
       target: object.constructor,
       propertyName,
